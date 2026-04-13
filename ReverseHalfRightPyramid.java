301f116f3405fb273ac655eb87b17aa9f6068d57
ShaikMunafSharif <169081792+ShaikMunafSharif@users.noreply.github.com>
2026-04-14 01:26:58 +0530

    Add files via upload

---
+++ ReverseHalfRightPyramid.java
@@ -0,0 +1,9 @@
+public class ReverseHalfRightPyramid {
+    public static void main(String[] args) {
+    System.out.println("* * * * *");
+    System.out.println("* * * * ");
+    System.out.println("* * *");
+    System.out.println("* *");
+    System.out.println("*");
+    }
+}
